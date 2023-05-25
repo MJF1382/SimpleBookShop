309a277028e5d52a4d35adf940e0d6fc6ee16f5e
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-25 06:08:30 -0700

    Adding base controller.

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleBookShop.Controllers
+{
+    public class BaseController : Controller
+    {
+        public const string Server_Error = "خطا در اتصال به سرور، لطفا بعدا دوباره امتحان کنید.";
+        public const string NotFound_Error = "آیتم مورد نظر یافت نشد.";
+    }
+}
