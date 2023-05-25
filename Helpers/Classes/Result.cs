d43057aba37d3af4987a62f819afbd441b8d8b9b
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-25 06:06:19 -0700

    Adding result class.

---
+++ Helpers/Classes/Result.cs
@@ -0,0 +1,29 @@
+﻿using SimpleBookShop.Helpers.Enums;
+
+namespace SimpleBookShop.Helpers.Classes
+{
+    public class Result
+    {
+        public ResultType Status { get; set; }
+        public object? Content { get; set; }
+        public string? ErrorText { get; set; }
+
+        public Result(ResultType status, object content, string errorText)
+        {
+            Status = status;
+            Content = content;
+            ErrorText = errorText;
+        }
+
+        public Result(ResultType status, string errorText)
+        {
+            Status = status;
+            ErrorText = errorText;
+        }
+
+        public Result(ResultType status)
+        {
+            Status = status;
+        }
+    }
+}
