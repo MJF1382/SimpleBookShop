1e474e9c69cbe628e8632c9e02c264e6acd4d29c
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-05-25 06:06:38 -0700

    Adding "BookViewModel".

---
+++ Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleBookShop.Models.ViewModels
+{
+    public class BookViewModel
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+    }
+}
